7e351182a34a888da94024b379bd0c9b970bbbe4
Aleksandra <chaschina.aleksandra@gmail.com>
2020-05-05 11:51:31 +0200

    update route api

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://8e253b4b.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://e9d98c06.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
